b2a3f7cf3e9d61b271a336ee5a293b52c1bf6c4d
galangriarulhaq <galangrz337@gmail.com>
2024-01-10 23:26:16 +0700

    adding functions to buttons

--- app/src/main/java/com/example/myapplication/IntroActivity.kt
+++ app/src/main/java/com/example/myapplication/IntroActivity.kt
@@ -1,11 +1,27 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class IntroActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var startBtn: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        startBtn = findViewById(R.id.startBtn)
+        startBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.startBtn -> {
+                val moveActivity = Intent(this@IntroActivity, MainActivity::class.java)
+                startActivity(moveActivity)
+            }
+        }
     }
 }
